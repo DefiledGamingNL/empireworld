133a4e540bdc3f3a22c04b3ec48b2b1031ccb1eb
Danny <dannyheijnen1991@gmail.com>
2023-02-07 19:00:55 +0100

    .

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,13 +2,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '@/styles/globals.scss'
 import React from 'react'
 import App from 'next/app'
-import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react'
-import store, { persistor } from '@/store/store'
+import {Provider} from 'react-redux'
+import {PersistGate} from 'redux-persist/integration/react'
+import store, {persistor} from '@/store/store'
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {fas} from '@fortawesome/free-solid-svg-icons'
+import {faMars, faVenus} from '@fortawesome/free-solid-svg-icons'
+
+library.add(fas, faMars, faVenus)
 
 class MyApp extends App {
     render() {
-        const { Component, pageProps } = this.props
+        const {Component, pageProps} = this.props
         return (
             <Provider store={store}>
                 <PersistGate persistor={persistor}>
--- src/pages/create-character.js
+++ src/pages/create-character.js
@@ -1,20 +1,59 @@
-import React, { useSelector } from 'react';
-import { useDispatch } from 'react-redux';
-import { createCharacter } from '@/actions/createCharacter';
+import React, {useState} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {createCharacter} from '@/actions/createCharacter';
+import {Col, Container, Form, Row} from "react-bootstrap";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
 
 const CharacterCreationPage = () => {
     const isLoggedIn = useSelector(state => state.isLoggedIn);
     const dispatch = useDispatch();
+    const [name, setName] = useState('');
+    const [gender, setGender] = useState('');
 
     const handleCreateCharacter = (character) => {
         dispatch(createCharacter(character));
     }
 
+    const handleNameChange = (e) => {
+        setName(e.target.value);
+    }
+
+    const handleGenderChange = (selectedGender) => {
+        setGender(selectedGender);
+    }
+
     return (
         isLoggedIn ?
+
             <div>
-                {/* Render character creation form here */}
-                <button onClick={() => handleCreateCharacter(/* character object */)}>Create Character</button>
+                {
+                    <Container>
+                        <Form>
+                            <Form.Group className='mb-3' controlId='formName'>
+                                <Form.Label>Character Name:</Form.Label>
+                                <Form.Control type='text' placeholder='Character Name' value={name}
+                                              onChange={handleNameChange}/>
+                            </Form.Group>
+
+                            <Form.Group className='mb-3' controlId='formGender'>
+                                <Form.Label>Gender:</Form.Label>
+                                <Row>
+                                    <Col xs={6}>
+                                        <FontAwesomeIcon icon='fa-solid fa-mars'/>
+                                    </Col>
+                                    <Col xs={6}>
+                                        <FontAwesomeIcon icon='fa-solid fa-venus'/>
+                                    </Col>
+                                </Row>
+
+                            </Form.Group>
+                        </Form>
+
+                        <button onClick={() => handleCreateCharacter(/* character object */)}>Create Character</button>
+                    </Container>}
+
+
             </div>
             :
             <div>
